a06e00f82529304830d14d61ebd45fb7170a1095
Janek Kurzydlo <janek.kurzydlo@gmail.com>
2018-06-29 00:10:23 +0200

    Remove unused files

--- src/views-js/LoginButton.js
+++ src/views-js/LoginButton.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Button from 'react-toolbox/lib/button/Button';
-import './LoginButton.css';
 
 const authorizeUrl = 'https://github.com/login/oauth/authorize'
 const clientId = 'a4d051aa767e07011a2f'
