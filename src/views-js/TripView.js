e5d48e163e8ecac5a5d161c5bf9751c70ee815e7
Dominik <bujasdominik@gmail.com>
2018-06-28 22:59:47 +0200

    tripView + Google Maps

--- src/views-js/TripView.js
+++ src/views-js/TripView.js
@@ -1,13 +1,55 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react'
+import {Map, InfoWindow, Marker, GoogleApiWrapper, Polyline} from 'google-maps-react';
+
+const AnyReactComponent = ({ text }) => <div>{ text }</div>;
+
 
 class TripView extends React.Component {
 
 
   render() {
-    return (
-    <div>Trip</div>
+
+      const triangleCoords = [
+          {lat: 25.774, lng: -80.190},
+          {lat: 18.466, lng: -66.118},
+          {lat: 32.321, lng: -64.757},
+          {lat: 25.774, lng: -80.190}
+      ];
+
+
+      const {selectedTrip} = this.props;
+      const style = {
+          width: '100%',
+          height: '100%'
+      }
+      return (
+            <div>
+                  <a href="#"><img className="card-img-top" src="http://via.placeholder.com/100x70" alt="" /></a>
+                  <div className="card-body">
+                      <h4 className="card-title">
+                          {selectedTrip.name}
+                      </h4>
+                  </div>
+                  <div>
+
+                      <Map
+                          google={this.props.google}
+                          style = {style}
+                          initialCenter={{
+                              lat: 40.854885,
+                              lng: -88.081807
+                          }}
+                          zoom={3}
+                          className={'map'}
+                      />
+                  </div>
+          </div>
+
     )
   }
 }
-export default TripView;
+export default GoogleApiWrapper({
+    key: ("AIzaSyBGDGARTFdB7pPRa_jX2M0Ow5NmbwmPWCQ")
+})(TripView);
 
